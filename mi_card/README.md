31268b3c42a896200de2ae05975401cf1e082b0e
Krittika-Saha <krittika.saha.dev@gmail.com>
2021-01-21 17:55:54 +0530

    started a new flutter app: mi card
    A Single Screen Personal Business Card App

---
+++ mi_card/README.md
@@ -0,0 +1,3 @@
+# mi_card
+
+A Single Screen Personal Business Card App
---
+++ mi_card/android/app/src/main/kotlin/krittika/mi_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package krittika.mi_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
