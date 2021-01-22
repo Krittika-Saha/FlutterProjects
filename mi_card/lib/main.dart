be6321f00248e277284ecc5902a06ac0fc8e45b3
Krittika-Saha <krittika.saha.dev@gmail.com>
2021-01-22 20:57:29 +0530

    Completed mi_card app

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -10,44 +10,86 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child:Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors:[
+                Colors.teal[700],
+                Colors.teal[500],
+                Colors.teal,
+                Colors.teal[300],
+                Colors.teal[300],
+                Colors.teal[300],
+              ],
+            )
+          ),
+          child: SafeArea(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                  width: 100.0,
-                  color: Colors.red,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/ghost.png'),
+                backgroundColor: Colors.white,
               ),
-              Container(
-                width: 100.0,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      height: 100.0,
-                      width: 100.0,
-                      color: Colors.yellow,
-                    ),
-                    Container(
-                      height: 100.0,
-                      width: 100.0,
-                      color: Colors.green
-                    )
-                  ]
+              Text("Krittika Saha",
+              style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Pacifico")),
+              Text("FLUTTER DEVELOPER",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.teal[100],
+                  fontFamily: "SourceSansPro",
+                  letterSpacing: 3,
+                  fontWeight: FontWeight.bold)),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal[100],
                 )
               ),
-              Container(
-                  width: 100.0,
-                  color: Colors.blue,
-              ),
-            ],
-          ),
+              Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 20.0,
+                    color: Colors.teal,
+                  ),
+                  title:Text(
+                      '+91 9007788182',
+                      style: TextStyle(
+                          color: Colors.teal[900],
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20)
+                  )
+              )),
+              Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  size: 20.0,
+                  color: Colors.teal,
+                ),
+                title:Text(
+                    'krittika.saha.dev@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20)
+              )
+              )
+              )],
+          )),
         ),
       ),
     );
   }
 }
-
