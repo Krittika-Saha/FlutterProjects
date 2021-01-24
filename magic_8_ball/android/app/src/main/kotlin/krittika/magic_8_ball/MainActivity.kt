61874463c8d308020d72a5768e6a116d62a61b2b
Krittika-Saha <krittika.saha.dev@gmail.com>
2021-01-24 15:59:14 +0530

    Challenge 1 completed :)

---
+++ magic_8_ball/android/app/src/main/kotlin/krittika/magic_8_ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package krittika.magic_8_ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ magic_8_ball/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: Center(
+            child: Text('Ask Me Anything'),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: BallPage(),
+      ),
+    ),
+  );
+}
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
